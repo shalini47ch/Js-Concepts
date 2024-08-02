96b75be83cd9a44daa99e7a9970a75e866914fda
Shalini Choudhary <60210475+shalini47ch@users.noreply.github.com>
2024-08-02 15:28:54 +0000

    slice splice along with other methods added

--- basics/array.js
+++ basics/array.js
@@ -31,3 +31,15 @@ console.log(newArray)
 //these methods need to give yes no statements
 console.log(newArray.includes(9))
 
+console.log(newArray.indexOf(14))
+//here the type changes to that of string
+console.log(typeof(newArray.join()))
+
+//slice doesnt manipulate the original array whereas splice manipulates the original array
+//in slice the last index is not included whereas in splice it is included
+newarr1=newArray.slice(1,3)
+console.log("A",newarr1)
+//in splice the last index is also included
+newarr2=console.log("B",newArray.splice(1,3))
+console.log(newArray) //here in case of splice the original array also gets modified 
+
