5dd7df8007b75a45d8c144f0a260bfc87337fb41
Shalini Choudhary <60210475+shalini47ch@users.noreply.github.com>
2024-08-02 14:25:10 +0000

    array operations added

---
+++ basics/array.js
@@ -0,0 +1,33 @@
+//here in the array we can store elements of different types that is all of them need not be numbers they can be strings,boolean or any other values as well.
+
+//All javascript copy operations promote shallow copy ,shallow copy means properties share the same reference whereas deep copy means the properties share different references
+
+const arr=[10,15,20,25]
+//other ways to declare the array
+const champions=["avengers","marvel"]
+const newArray=new Array(5,6,7,8,9)
+console.log(arr[0])
+console.log(newArray)
+
+//some methods to perform operations on the array
+//concat > meant to add two arrays here a new array is created
+
+const arr1=[12,16,18,19,20]
+const arr2=[25,28,29,31,22]
+const arr3=arr1.concat(arr2)
+console.log(arr3)
+
+//similarly use the push method to add the elements to an array
+//pop removes the element from the last
+newArray.push(12)
+newArray.push(14)
+newArray.push(16)
+newArray.pop()
+//unshift is a method that helps to add an element to the beginning of the array
+newArray.unshift(6)
+//shift is a method that helps to remove an element from the beginning of the array
+newArray.shift() 
+console.log(newArray)
+//these methods need to give yes no statements
+console.log(newArray.includes(9))
+
