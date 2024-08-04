a5287cef7731e1a9351ece7b22262e27b5f62912
Shalini Choudhary <60210475+shalini47ch@users.noreply.github.com>
2024-08-04 15:28:05 +0000

    object.assign and spread operators used

--- basics/objects1.js
+++ basics/objects1.js
@@ -11,10 +11,25 @@ const regularUser={
 //here we have created objects of objects
 console.log(regularUser.fullname?.userfullname?.firstname)
 
-//agar do tin objects hai to unko aapas mei combine karne ke liye Object.assign use karsakte hai
+//this is use to combine two three objects together
 
 const obj1={1:"a",2:"b"}
 const obj2={3:"c",4:"d"}
 
-const obj3=Object.assign({},obj1,obj2)
+// const obj3=Object.assign({},obj1,obj2)
+// console.log(obj3) this is used less 
+
+//the most commomly used are the spread operators
+const obj3={...obj1,...obj2}
 console.log(obj3)
+
+const newuser={
+    name:"abc",
+    age:12,
+    gender:"F"
+}
+
+console.log(Object.keys(newuser)) //here the keys will be returned in the form of arrays
+console.log(Object.values(newuser)) //here it will return the values of the user
+//we can also use Object.entries here it will return an array of array
+console.log(Object.entries(newuser))
